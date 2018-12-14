d087d3be81d5a6829ceb67e8dd7e4615571b67b1
Zachary Lozano <zlozano@atlassian.com>
2018-12-14 11:59:21 -0600

    add project skeleton

---
+++ pkg/types/log.go
@@ -0,0 +1,10 @@
+package types
+
+import (
+	"context"
+
+	"bitbucket.org/atlassian/logevent"
+)
+
+// LoggerProvider extracts a logger from context.
+type LoggerProvider func(ctx context.Context) logevent.Logger
