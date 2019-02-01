9e183ef55588aa195b53fee92636969dc33c9dbd
Kevin Conway <kconway@atlassian.com>
2019-02-01 16:56:26 -0600

    Add timestamp insertion for marker file content

--- pkg/marker/s3.go
+++ pkg/marker/s3.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -14,36 +15,37 @@ import (
 
 const inProgressSuffix = "_in_progress"
 
-// ProgressMarker is an implementation of Marker which allows for marking/unmarking of graphs in progress
+// ProgressMarker is an implementation of Marker which allows for marking/unmarking of diffs in progress
 type ProgressMarker struct {
 	Bucket   string
 	Client   s3iface.S3API
 	uploader s3manageriface.UploaderAPI
 	once     sync.Once
+	now      func() time.Time
 }
 
 // Mark flags the graph identified by key as being "in progress"
 func (m *ProgressMarker) Mark(ctx context.Context, key string) error {
-	m.once.Do(m.initUploader)
+	m.once.Do(func() {
+		m.uploader = s3manager.NewUploaderWithClient(m.Client)
+	})
+	now := m.now
+	if now == nil {
+		now = time.Now
+	}
 	_, err := m.uploader.UploadWithContext(ctx, &s3manager.UploadInput{
 		Bucket: aws.String(m.Bucket),
 		Key:    aws.String(key + inProgressSuffix),
-		Body:   bytes.NewReader([]byte("")),
+		Body:   bytes.NewReader([]byte(now().Format(time.RFC3339Nano))),
 	})
 	return err
 }
 
-// Unmark flags the graph identified by key as not being "in progress"
+// Unmark flags the diff identified by key as not being "in progress"
 func (m *ProgressMarker) Unmark(ctx context.Context, key string) error {
 	_, err := m.Client.DeleteObjectWithContext(ctx, &s3.DeleteObjectInput{
 		Bucket: aws.String(m.Bucket),
 		Key:    aws.String(key + inProgressSuffix),
 	})
 	return err
-}
-
-func (m *ProgressMarker) initUploader() {
-	if m.uploader == nil {
-		m.uploader = s3manager.NewUploaderWithClient(m.Client)
-	}
 }
